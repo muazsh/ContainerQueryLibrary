faada2b29b19fa806d68dced59e86f8cecab62ef
MOUAZ\Mouaz <moaazshamia@gmail.com>
2021-04-06 19:16:01 +0200

    Update statement

--- ContainerQueryLibrary/ContainerQueryLibrary.h
+++ ContainerQueryLibrary/ContainerQueryLibrary.h
@@ -30,8 +30,8 @@ namespace cql
 	*  Returns: A container of elements that satisfy the predicate.
 	* 
 	*  Usage:
-	*  list<int> ls{1,2,3,4,5}
-	*  auto predicate = [](const int& v){ return v%2 == 0; }
+	*  list<int> ls{1,2,3,4,5};
+	*  auto predicate = [](const int& v){ return v%2 == 0; };
 	*  auto res = Where(ls, predicate);
 	* 
 	*  Minimum c++ standard: c++11.
@@ -48,12 +48,44 @@ namespace cql
 		return result;
 	}
 
+	/* Update Statement:
+	*  It allows to update a container based on an updating function.
+	*
+	*  Usage:
+	*  list<int> ls{1,2,3,4,5};
+	*  auto setFunc = [](int& v){ v = 10; };
+	*  Update(ls, setFunc);
+	*/
+	template<typename TContainer, typename TSetFunc>
+	void Update(TContainer& container, const TSetFunc& setFunc)
+	{
+		for (auto itr = container.begin(); itr != container.end(); ++itr)
+			setFunc(*itr);
+	}
+
+	/* Update Statement:
+	*  It allows to update a container based on an updating function and a predicate.
+	*
+	*  Usage:
+	*  list<int> ls{1,2,3,4,5};
+	*  auto set = [](int& v){ v = 10; };
+	*  auto predicate = [](const int& v){ return v%2 == 0; };
+	*  Update(ls, predicate, set);
+	*/
+	template<typename TContainer, typename TFunc, typename TSetFunc>
+	void Update(TContainer& container, const TFunc& predicate, const TSetFunc& setFunc)
+	{
+		for (auto itr = container.begin(); itr != container.end(); ++itr)
+			if (predicate(*itr))
+				setFunc(*itr);
+	}
+
 	/* Distinct:
 	* 
 	*  Returns: a distinct container of the original one.
 	*  
 	*  Usage: 
-	*  std::list<int> ls{ 11,11,2,2,3,5,6 };
+	*  list<int> ls{ 11,11,2,2,3,5,6 };
 	*  auto res = Distinct(ls);
 	*/
 	template<typename TContainer>
@@ -74,8 +106,8 @@ namespace cql
 	* 
 	*  Usage:
 	*  struct MyStruct{ int x,y; };
-	*  std::vector<MyStruct> ls{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
-	*  auto func = [](const MyStruct& l, const MyStruct& r) { return l.y < r.y; }
+	*  vector<MyStruct> ls{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
+	*  auto func = [](const MyStruct& l, const MyStruct& r) { return l.y < r.y; };
 	*  OrderBy(ls, func);
 	* 
 	*  Minimum c++ standard: c++17.
@@ -103,8 +135,8 @@ namespace cql
 	* 
 	*  Usage:
 	*  struct MyStruct{ int x,y; };
-	*  std::vector<MyStruct> ls{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
-	*  auto func = [](const MyStruct& myStruct) { return myStruct.x; }
+	*  vector<MyStruct> ls{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
+	*  auto func = [](const MyStruct& myStruct) { return myStruct.x; };
 	*  auto res = GroupBy(ls, func);
 	* 
 	*  Minimum c++ standard: c++14.
@@ -133,10 +165,10 @@ namespace cql
 	*  Usage:
 	*  struct MyStruct1{ int x,y; };
 	*  struct MyStruct2{ int w,z; };
-	*  std::vector<MyStruct1> vec{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
-	*  std::list<MyStruct2> ls{ {1,1} ,{3,4} ,{1,4}, {2,8}, {1,1} };
-	*  auto func1 = [](const MyStruct1& myStruct) { return myStruct.y; }
-	*  auto func2 = [](const MyStruct2& myStruct) { return myStruct.z; }
+	*  vector<MyStruct1> vec{ {1,4} ,{3,4} ,{1,4}, {2,7}, {1,1}};
+	*  list<MyStruct2> ls{ {1,1} ,{3,4} ,{1,4}, {2,8}, {1,1} };
+	*  auto func1 = [](const MyStruct1& myStruct) { return myStruct.y; };
+	*  auto func2 = [](const MyStruct2& myStruct) { return myStruct.z; };
 	*  auto res = Join(vec, ls, func1, func2);
 	* 
 	*  Minimum c++ standard: c++14.
